167455de979f706b7061e0ee3e7038a3df3f7a00
Jim Phieffer <phiefferj24@columbusacademy.org>
2022-08-10 03:39:29 -0400

    Initial Commit

---
+++ Shared/CAApp.swift
@@ -0,0 +1,17 @@
+//
+//  CAApp.swift
+//  Shared
+//
+//  Created by Jim Phieffer on 8/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
